93aa5ea8b9dca55af2d733f69086bb5f499f899e
Adrian Ginalski <adrian.ginalski@comarch.com>
2023-10-06 16:02:11 +0200

    Finishing configuration

--- dockerfile
+++ dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.9
 ENV APP_HOME=/home/app/celeryApp
+ENV MAIN_CONTENT_HOME=/home/app/celeryApp/main_content
 ENV POETRY_VERSION=1.5.1
 
 
@@ -16,4 +17,5 @@ COPY . $APP_HOME
 ENV FLASK_APP=app.py
 EXPOSE 5000
 
+WORKDIR $MAIN_CONTENT_HOME
 CMD ["python3", "-m" , "flask", "run", "--host", "0.0.0.0", "--port", "5000"]
--- main_content/app.py
+++ main_content/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, flash, url_for, redirect
+from celery import Celery
 from pymongo.errors import ServerSelectionTimeoutError
+from time import sleep
 
 from db_config import connection
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
+app.config['CELERY_BROKER_URL'] = os.environ['CELERY_BROKER_URL']
+app.config['CELERY_RESULT_BACKEND'] = os.environ['CELERY_RESULT_BACKEND']
+
+celery = Celery('worker', broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+celery.conf.update(app.config)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -14,9 +21,12 @@ def fill_form():
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
+        if not name or not email or not message:
+            flash("Empty fields...", "error")
+            return redirect(url_for('fill_form'))
         try:
-            db = connection()
-            db.insert_one({'name': name, 'email': email, 'message': message})
+            parallel_task.delay()
+            save_to_db(name, email, message)
         except ServerSelectionTimeoutError:
             flash("Cannot connect to the server...", "error")
             return redirect(url_for('fill_form'))
@@ -24,6 +34,16 @@ def fill_form():
         return redirect(url_for('fill_form'))
     else:
         return render_template('main_form.html')
+
+
+@celery.task
+def parallel_task():
+    sleep(10)
+
+
+def save_to_db(name, email, message):
+    db = connection()
+    db.insert_one({'name': name, 'email': email, 'message': message})
 
 
 if __name__ == '__main__':
